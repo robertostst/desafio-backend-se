d6d2887fd0ea1bd01ca6a9bb0cbcc3ba317dc865
Roberto Aguiar Jr <roberto.junior@mazzafc.tech>
2023-08-29 16:11:38 -0300

    Frontend

---
+++ assets/index.js
@@ -0,0 +1,64 @@
+function calcular(event, form) {
+    event.preventDefault();
+    const inputCep = form.cep;
+// dados a serem enviados pela solicitação POST
+    let _data = {
+        "pedidoParaRateio": {
+            "itens": [
+              {
+                "descricao": form.descricao_1.value,
+                "valor": form.valor_1.value,
+                "cliente": {
+                    "nome": form.cliente_1.value
+                }
+              },
+              {
+                "descricao": form.descricao_2.value,
+                "valor": form.valor_2.value,
+                "cliente": {
+                    "nome": form.cliente_2.value
+                }
+              },
+              {
+                "descricao": form.descricao_3.value,
+                "valor": form.valor_3.value,
+                "cliente": {
+                    "nome": form.cliente_3.value
+                }
+              }
+            ],
+            "valorTaxaEntrega": form.taxa_entrega.value,
+            "valorDesconto": form.valor_desconto.value
+          }
+          ,
+          "chave": form.chave.value
+    }
+    
+    fetch('http://localhost:8090/api/rateio/calculos?gerarLinkPagamento=true', {
+        method: "POST",
+        body: JSON.stringify(_data),
+        headers: {"Content-type": "application/json; charset=UTF-8"}
+    })
+    .then(response => response.json()) 
+    //.then(json => console.log(json))
+    .then(json => mostrarResposta(json))
+    .catch(err => console.log(err));
+}
+
+function mostrarResposta(dados) {
+    const lista = dados.rateio.linksPagamento;
+    let mensagem = ``;
+
+    for (i in lista){
+        mensagem += `
+            Pessoa: ${lista[i].identificacaoDevedor}
+            Valor a pagar: ${lista[i].valorAPagar}
+            Link Pagamento: ${lista[i].pixLink}
+
+        `;
+    }
+
+    alert(mensagem);
+    
+    //alert(JSON.stringify(dados));
+}
